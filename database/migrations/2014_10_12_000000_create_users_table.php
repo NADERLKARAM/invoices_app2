91b0c11e7d38d3e73a53bac05fcbe3e0f2c4a443
nader <naedrkamal4@gmail.com>
2024-06-27 06:55:51 +0300

    .

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->text('roles_name')->default('admin');
+            $table->text('roles_name')->default('user');
             $table->string('Status', 10);
             $table->rememberToken();
             $table->timestamps();
